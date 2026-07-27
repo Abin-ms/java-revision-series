55f4e323156e98360b83075a322588f3bd57da27
Abin-ms <abinn.mss@gmail.com>
2026-07-27 21:48:27 +0530

    pushing practice codes

---
+++ day4/innerclassex1.java
@@ -0,0 +1,51 @@
+package day4;
+
+ class innerclassex1 {
+    public static void main(String[] args){
+        phone pp = new phone();
+        phone.display dd = pp.new display();
+        phone.camera cc = pp.new camera();
+
+        dd.setValue1(15000, "samsung");
+        dd.getvalue1();
+        cc.setValue2(120, "Sony");
+        cc.getvalue2();
+
+
+        
+
+    }
+}
+
+class phone{
+    class display{
+        int rate;
+        String manufacturer;
+         public void setValue1(int rate , String manufacturer){
+        this.rate = rate;
+        this.manufacturer = manufacturer;
+
+    }
+          public void getvalue1(){
+        System.out.println("the display rate is : "+rate);
+        System.out.println("Manufacturer is : "+manufacturer);
+    }
+
+    }
+    
+   
+   
+    public class camera{
+        int megapixel;
+        String manufacturer;
+        public void setValue2(int megapixel , String manufacturer){
+            this.megapixel = megapixel;
+            this.manufacturer = manufacturer;
+        }
+        public void getvalue2(){
+            System.out.println("The camera is of "+megapixel+"MP");
+            System.out.println("Manufatured by : "+manufacturer);
+        }
+
+    }
+}
