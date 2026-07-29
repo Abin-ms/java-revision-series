e72f7ef9de357e3985cae2956502c5a7aa1ef493
Abin-ms <abinn.mss@gmail.com>
2026-07-29 18:38:41 +0530

    ffd

---
+++ day5/divbyzeroexception.java
@@ -0,0 +1,12 @@
+package day5;
+
+ class divbyzeroexception {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 0;
+        System.out.println(x/y);
+        System.out.println("Program is executing..");
+    }
+}
+
+//this is a visualization of divide by zero where the exception causes the pgm to terminate execution , and we will solve the problem using try catch block in another file .
