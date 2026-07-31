9c0d9b7fa4bfe2401c3bffc1ace6183d053c4865
Abin-ms <abinn.mss@gmail.com>
2026-07-31 17:03:02 +0530

    pushing new practice code files

---
+++ day6/listex1.java
@@ -0,0 +1,19 @@
+package day6;
+
+import java.util.*;
+import java.util.ArrayList;
+
+public class listex1 {
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Ball");
+        list.add("cat");
+        list.add("Dog");
+
+        System.out.println(list);
+    }
+    
+}
