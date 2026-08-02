571a12519af392ff2189130f680c689f21ffdfc9
Abin-ms <abinn.mss@gmail.com>
2026-08-02 23:27:19 +0530

    pushing new file of day7

---
+++ day7/linkedlistex.java
@@ -0,0 +1,16 @@
+package day7;
+
+import java.util.LinkedList;
+
+public class linkedlistex {
+    public static void main(String[] args){
+        LinkedList<String> movies = new LinkedList<>();
+
+        movies.add("Interstellar");
+        movies.add("Incendies");
+        movies.add("Arrival");
+        movies.add("Kill bill");
+        movies.add("Pulp Fiction");
+        
+    }
+}
