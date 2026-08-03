6f43318185083ed498e0d15e2bf00bed9f0aadc4
Abin-ms <abinn.mss@gmail.com>
2026-08-03 22:00:22 +0530

    made some changes in it

--- day7/vector_vs_arraylist.java
+++ day7/vector_vs_arraylist.java
@@ -9,7 +9,15 @@ public class vector_vs_arraylist {
         List<String> list = new ArrayList<>(Arrays.asList("abin","jibin"));
 
         System.out.print(list);
-        
+        List<String> list2 = new ArrayList<>();
+
+        list2.add("hellloo");
+        list2.remove(0);
+        System.out.println(list2);
+        // list2.add("hellloo");
+        // list2.remove(0);
+        // System.out.println(list2);  Here multiple threads can access same file or resource but there is no thread safety.That is ArrayList is not Synchronized.
+       // But vector list is Synchronized and Thread safe.
 
         
     }
