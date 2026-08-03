2da6e8852ee68194d58a67878b9c0f9b756c61e5
Abin-ms <abinn.mss@gmail.com>
2026-08-03 21:54:57 +0530

    adding new practice file

---
+++ day7/vector_vs_arraylist.java
@@ -0,0 +1,16 @@
+package day7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class vector_vs_arraylist {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>(Arrays.asList("abin","jibin"));
+
+        System.out.print(list);
+        
+
+        
+    }
+}
