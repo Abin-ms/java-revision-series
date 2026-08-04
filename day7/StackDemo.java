a64658d319bbc8764176c25c30440fa856acec9b
Abin-ms <abinn.mss@gmail.com>
2026-08-04 12:10:16 +0530

    adding new practice files

---
+++ day7/StackDemo.java
@@ -0,0 +1,16 @@
+package day7;
+
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args){
+        Stack<String> books = new Stack<>();
+        books.push("Java");
+        books.push("SQL");
+        books.push("Spring Boot");
+        books.push("Learn Spring Boot");
+        books.push("Revise Collections");
+
+        System.out.println(books);
+    }
+}
