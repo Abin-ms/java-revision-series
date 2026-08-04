4095e13a7add46b75c06993e741fbcaca1987e38
Abin-ms <abinn.mss@gmail.com>
2026-08-04 10:47:42 +0530

    adding a new practice file

---
+++ day7/vectorr_testexample.java
@@ -0,0 +1,13 @@
+package day7;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+public class vectorr_testexample {
+    public static void main(String[] args){
+        Vector<String> list = new Vector<>(Arrays.asList("Java","Python","C++","Javascript","GO"));
+
+        System.out.println(list);
+        
+    }
+}
