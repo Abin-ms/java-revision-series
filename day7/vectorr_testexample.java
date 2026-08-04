f12a94880235b679fe147c64cb60fd7b28cd0f5a
Abin-ms <abinn.mss@gmail.com>
2026-08-04 11:05:33 +0530

    pushing file in mid codin

--- day7/vectorr_testexample.java
+++ day7/vectorr_testexample.java
@@ -7,9 +7,20 @@ public class vectorr_testexample {
     public static void main(String[] args){
         Vector<String> list = new Vector<>(Arrays.asList("Java","Python","C++","Javascript","GO"));
 
-        System.out.println(list);
+        System.out.print("initial printing : "+list);
 
         list.add(2,"Kotlin");
+
+        System.out.print("\nprinting after adding kotlin into 2 nd position : "+list);
+
+        int valueOFGo = list.indexOf("GO");
+
+        list.set(valueOFGo, "Rust");
+
+        System.out.println("\nPrinting after replacing GO"+list);
+
+        list.remove("Python");
+
         
     }
 }
