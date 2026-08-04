4e091fa51ff5fef558feb802200cec3325335b48
Abin-ms <abinn.mss@gmail.com>
2026-08-04 11:18:45 +0530

    final code pushing

--- day7/vectorr_testexample.java
+++ day7/vectorr_testexample.java
@@ -5,32 +5,32 @@ import java.util.Iterator;
 import java.util.Vector;
 
 public class vectorr_testexample {
-    public static void main(String[] args){
-        Vector<String> list = new Vector<>(Arrays.asList("Java","Python","C++","Javascript","GO"));
+    public static void main(String[] args) {
+        Vector<String> list = new Vector<>(Arrays.asList("Java", "Python", "C++", "Javascript", "GO"));
 
-        System.out.print("initial printing : "+list);
+        System.out.print("initial printing : " + list);
 
-        list.add(2,"Kotlin");
+        list.add(2, "Kotlin");
 
-        System.out.print("\nprinting after adding kotlin into 2 nd position : "+list);
+        System.out.print("\nprinting after adding kotlin into 2 nd position : " + list);
 
         int valueOFGo = list.indexOf("GO");
 
         list.set(valueOFGo, "Rust");
 
-        System.out.println("\nPrinting after replacing GO"+list);
+        System.out.println("\nPrinting after replacing GO" + list);
 
         list.remove("Python");
 
-        System.out.println("Python is removed : " +list);
+        System.out.println("Python is removed : " + list);
 
         System.out.println("Printing using normal for loop");
-        for(int i = 0 ; i < list.size() ; i++){
+        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
 
         System.out.println("printing using enhanced for loop : ");
-        for(String li : list){
+        for (String li : list) {
             System.out.println(li);
         }
 
@@ -38,16 +38,23 @@ public class vectorr_testexample {
 
         while (itr.hasNext()) {
             System.out.println(itr.next());
-            
+
         }
 
-        System.out.println("printing the first element : "+list.firstElement());
+        System.out.println("printing the first element : " + list.firstElement());
 
-        System.out.println("printing the last element"+list.lastElement());
+        System.out.println("printing the last element" + list.lastElement());
 
-        System.out.println("Total number of elements is : "+list.size());
+        System.out.println("Total number of elements is : " + list.size());
 
+        while (itr.hasNext()) {
+            if (itr.next().equals("Java")) {
+                System.out.println("Yes,java is in the list.");
+            } else {
+                System.out.println("Java is not in the list");
+            }
 
-        
+        }
+
     }
 }
