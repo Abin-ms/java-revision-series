fc314cfba29277936f74e230aa432208e5af1809
Abin-ms <abinn.mss@gmail.com>
2026-08-06 16:56:31 +0530

    code showcasing the use of addAll()

---
+++ day8/hashsetexample2.java
@@ -0,0 +1,23 @@
+package day8;
+
+import java.util.HashSet;
+
+public class hashsetexample2 {
+    public static void main(String[] args){
+        HashSet <String> language = new HashSet<>();
+
+        language.add("C");
+        language.add("C++");
+        language.add("Java");
+        language.add("Python");
+
+        System.out.println("Hashset : "+language);
+
+        HashSet <String> language2 = new HashSet<>();
+        //using addAll() method.
+        language2.addAll(language);
+
+        System.out.println("The copied new hashset : "+language2);
+
+    }
+}
