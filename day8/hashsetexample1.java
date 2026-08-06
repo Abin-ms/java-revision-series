a633cb6a993c570637b3011e8d7aa1593608c70f
Abin-ms <abinn.mss@gmail.com>
2026-08-06 16:50:47 +0530

    code on how to add elements into the Hashset

---
+++ day8/hashsetexample1.java
@@ -0,0 +1,18 @@
+package day8;
+
+import java.util.HashSet;
+
+public class hashsetexample1 {
+    public static void main(String[] args){
+        HashSet <Integer> evenNum = new HashSet<>();
+
+        evenNum.add(2);
+        evenNum.add(4);
+        evenNum.add(6);
+        evenNum.add(8);
+
+        System.out.println("Hashset : "+evenNum);
+        // adding element into the hashset.
+        
+    }
+}
