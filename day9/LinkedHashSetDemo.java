c8b3d666bf9a7bda3007464032c072af22fa1568
Abin-ms <abinn.mss@gmail.com>
2026-08-07 22:50:44 +0530

    commit no - 3

--- day9/LinkedHashSetDemo.java
+++ day9/LinkedHashSetDemo.java
@@ -14,5 +14,14 @@ public class LinkedHashSetDemo {
         sett.add("Tenet");
 
         System.out.println(sett);
+
+        System.out.println("Total number of movies : "+sett.size());
+        if(sett.contains("Tenet")){
+            System.out.println("Tenet is present in the list.");
+        }
+
+        System.out.println("Inception removed : "+sett.remove("Inception"));
+
+        System.out.println("Updated set : "+sett);
     }
 }
