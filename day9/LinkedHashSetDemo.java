869341ca7821f724b748317ccffce67951f46af2
Abin-ms <abinn.mss@gmail.com>
2026-08-07 22:43:57 +0530

    commit no - 1

---
+++ day9/LinkedHashSetDemo.java
@@ -0,0 +1,13 @@
+package day9;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args){
+        Set <String> sett = new LinkedHashSet<>();
+
+        sett.add("Interstellar");
+        sett.add("Inception");
+    }
+}
