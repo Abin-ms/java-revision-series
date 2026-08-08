8cd6cc1dc37624d382a3a461631b50b371dbd448
Abin-ms <abinn.mss@gmail.com>
2026-08-08 17:25:38 +0530

    final commit in the file

--- day9/TreeSetDemo.java
+++ day9/TreeSetDemo.java
@@ -1,11 +1,11 @@
 package day9;
 
-import java.util.Set;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class TreeSetDemo {
-    public static void main(String[] args){
-        Set<Integer> numbers = new TreeSet<>();
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
 
         numbers.add(75);
         numbers.add(90);
@@ -15,9 +15,30 @@ public class TreeSetDemo {
         numbers.add(95);
         numbers.add(70);
 
-        System.out.println("Initial set : "+numbers);
-        numbers.first();
-        numbers.last();
-        
+        System.out.println("Initial set : " + numbers);
+        System.out.println("Initial set : " + numbers.first());
+        System.out.println("Largest element : " + numbers.last());
+        System.out.println("lower than 75 : " + numbers.lower(75));
+        System.out.println("higher than 75 : " + numbers.higher(75));
+        System.out.println("using ceiling method : " + numbers.ceiling(76));
+        System.out.println("using floor function : " + numbers.floor(76));
+
+        if (numbers.contains(85))
+            numbers.remove(85);
+        if (!numbers.contains(85))
+            System.out.println("85 is deleted.");
+
+        System.out.println("Updated set : " + numbers);
+
+        System.out.println("Iterating using enhanced for loop");
+        for (Integer num : numbers) {
+            System.out.print(num + "  ");
+        }
+        System.out.println("\nPrinting using Iterator");
+        Iterator<Integer> itr = numbers.iterator();
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + "  ");
+        }
+
     }
 }
