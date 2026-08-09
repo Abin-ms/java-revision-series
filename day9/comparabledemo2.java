554f678bc7eae8bd99fef0f0f3c738e176df3e0d
Abin-ms <abinn.mss@gmail.com>
2026-08-09 17:47:43 +0530

    final commit in the file

--- day9/comparabledemo2.java
+++ day9/comparabledemo2.java
@@ -1,25 +1,36 @@
 package day9;
 
+import java.util.TreeSet;
+
 public class comparabledemo2 {
+
     public static void main(String[] args) {
 
+        TreeSet<Student> students = new TreeSet<>();
+
+        students.add(new Student(103, "John"));
+        students.add(new Student(101, "Abin"));
+        students.add(new Student(102, "Rahul"));
+        students.add(new Student(103, "Jibi"));
+        students.add(new Student(105, "Abin"));
+
+        System.out.println(students);
     }
 }
 
 class Student implements Comparable<Student> {
+
     int id;
     String name;
 
     Student(int id, String name) {
         this.id = id;
         this.name = name;
-
     }
 
     @Override
-    public String compareTo(Student other) {
-
-        return String.compare(this.name, other, name);
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
     }
 
     @Override
@@ -27,5 +38,4 @@ class Student implements Comparable<Student> {
         return "Student{id=" + id +
                 ", name='" + name + "'}";
     }
-
 }
