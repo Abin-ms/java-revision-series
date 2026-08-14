7b567e36bcee9a3ccc50b5c947f315c37776b6b5
Abin-ms <abinn.mss@gmail.com>
2026-08-14 23:10:41 +0530

    fixed a bug

--- day2/check_array_length4.java
+++ day2/check_array_length4.java
@@ -1,8 +1,12 @@
 public class check_array_length4 {
     public static void main(String[] args) {
-        int[] num = { 1, 2, 3, 4, 5, 6 };
+        int[] num = { 22, 23, 31, 41, 53, 1 };
         int i = 0;
+        int a = 1;
         for (int item : num) {
+            
+            System.out.println( "item no - " +a+ ": "+item);
+            a = a+1;
             i = i + 1;
         }
         System.out.println("the size of the array is:" + i);
