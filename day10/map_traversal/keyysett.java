4c92805e913d0963ff403f33d5fae02f023a8ee0
Abin-ms <abinn.mss@gmail.com>
2026-08-17 20:01:19 +0530

    example pgm for keySet() , values() , entrySet()

---
+++ day10/map_traversal/keyysett.java
@@ -0,0 +1,35 @@
+package day10.map_traversal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class keyysett {
+    public static void main(String[] args){
+        Map<Integer,String> stud = new HashMap<>();
+
+        stud.put(101, "Abin");
+        stud.put(102, "Jibin");
+        stud.put(103, "Sisu");
+        stud.put(104, "sulu");
+        stud.put(105, "Blaa");
+
+        System.out.println(stud);
+        System.out.println(stud.keySet());
+        System.out.println(stud.values());
+        System.out.println(stud.entrySet());
+
+        System.out.println("keySet() using for loop");
+        for(Integer id : stud.keySet()){
+            System.out.print(id+" ");
+        }
+        System.out.println("\nvalues() using for loop.");
+        for(String name : stud.values()){
+            System.out.print(name+" ");
+        }
+
+        System.out.println("\nenntrySet() using for loop");
+        for(Map.Entry<Integer,String> entry : stud.entrySet()){
+            System.out.println("id : "+entry.getKey()+", value :"+entry.getValue());
+        }
+    }
+}
--- day10/treemapdemoo.java
+++ day10/treemapdemoo.java
@@ -7,18 +7,18 @@ public class treemapdemoo {
 
     public static void main(String[] args) {
 
-       
         Comparator<Student1> byMarksThenName = Comparator
                 .comparingInt((Student1 s) -> s.marks)
                 .thenComparing(s -> s.name);
 
         TreeMap<Student1, String> students = new TreeMap<>(byMarksThenName);
 
-        
         students.put(new Student1(103, "John", 85), "Student 1");
         students.put(new Student1(101, "Abin", 92), "Student 2");
-        students.put(new Student1(102, "Rahul", 85), "Student 3"); // Same marks as John -> sorted by name ("John" before "Rahul")
-        students.put(new Student1(104, "Alice", 85), "Student 4"); // Same marks as John/Rahul -> sorted by name ("Alice" first)
+        students.put(new Student1(102, "Rahul", 85), "Student 3"); // Same marks as John -> sorted by name ("John"
+                                                                   // before "Rahul")
+        students.put(new Student1(104, "Alice", 85), "Student 4"); // Same marks as John/Rahul -> sorted by name
+                                                                   // ("Alice" first)
 
         students.forEach((key, value) -> System.out.println(key + " => " + value));
     }
