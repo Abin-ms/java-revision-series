55f63cf36d55b9c5bfc387f5653aa8373f7db4d5
Abin-ms <abinn.mss@gmail.com>
2026-08-18 16:42:30 +0530

    upperbounded wildcard example

---
+++ day11/widcards/uppboundex.java
@@ -0,0 +1,21 @@
+package day11.widcards;
+
+import java.util.*;
+
+public class uppboundex {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 30);
+        List<Double> decimals = Arrays.asList(10.5, 20.5, 30.5);
+
+        displayNumbers(numbers);
+        displayNumbers(decimals);
+
+    }
+
+    public static void displayNumbers(List<? extends Number> list) {
+        for (Number n : list) {
+            System.out.println(n);
+        }
+        System.out.println();
+    }
+}
