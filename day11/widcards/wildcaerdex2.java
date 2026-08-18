a81ec3577d41c64dacd6d40be720574ee77f2469
Abin-ms <abinn.mss@gmail.com>
2026-08-18 10:12:21 +0530

    example test case of unbounded wildcards

--- day11/widcards/wildcaerdex2.java
+++ day11/widcards/wildcaerdex2.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class wildcaerdex2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<String> names = Arrays.asList("Abin", "Jibin", "Sulu");
 
         List<Integer> numbers = Arrays.asList(10, 20, 30);
         display(numbers);
         display(names);
     }
-    public static void display(List<?> list){
-        for(Object value : list){
+
+    public static void display(List<?> list) {
+        for (Object value : list) {
             System.out.println(value);
         }
+        // list.add("Java");
         System.out.println();
 
     }
