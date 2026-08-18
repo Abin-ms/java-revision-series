71c9793bcb39c00d594bfbeffc47625ca6d24cfd
Abin-ms <abinn.mss@gmail.com>
2026-08-18 23:52:06 +0530

    example of lambda expression created with the help of functional interface

---
+++ day12/lambdausingfninterface.java
@@ -0,0 +1,19 @@
+package day12;
+
+public class lambdausingfninterface {
+    public static void main(String[] args) {
+        Calculator Addition = (a, b) -> a + b;
+        Calculator Subtraction = (a, b) -> a - b;
+        Calculator Multiplication = (a, b) -> a * b;
+
+        System.out.println(Addition.operate(10, 5));
+        System.out.println(Subtraction.operate(10, 5));
+        System.out.println(Multiplication.operate(10, 5));
+    }
+
+    @FunctionalInterface
+    interface Calculator {
+
+        int operate(int a, int b);
+    }
+}
