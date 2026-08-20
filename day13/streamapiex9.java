35d262cf3273aa5e30a17a596b96fa099ff69bfe
Abin-ms <abinn.mss@gmail.com>
2026-08-20 18:35:37 +0530

    example case of using toSet()

---
+++ day13/streamapiex9.java
@@ -0,0 +1,18 @@
+package day13;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class streamapiex9 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 10, 30, 20, 40);
+
+        Set<Integer> result = numbers.stream()
+                .collect(Collectors.toSet());
+
+        System.out.println(result);
+
+    }
+}
