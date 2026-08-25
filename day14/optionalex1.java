8b707e43d7bec7d5c1ed041d3a7dfc9eae48569d
Abin-ms <abinn.mss@gmail.com>
2026-08-25 21:08:14 +0530

    case showing why Optional was introduced or used / points out the problem which optional can solve

--- day14/optionalex1.java
+++ day14/optionalex1.java
@@ -2,6 +2,13 @@ package day14;
 
 public class optionalex1 {
     public static void main(String[] args){
-        
+        String[] words = new String[10];
+        String word = words[5].toLowerCase();
+        System.out.println(word);
+
+        //implementation showing why optional was introduced.
+        //if we run this code we will get Exception in thread "main" java.lang.NullPointerException - cause the String is empty.
+        //we look how optional can be used to handle the nullPointerException.
+
     }
 }
