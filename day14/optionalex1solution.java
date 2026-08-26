a98d8c296342e000a7092416cb8d1d745c237b57
Abin-ms <abinn.mss@gmail.com>
2026-08-26 14:29:37 +0530

    huhhh

--- day14/optionalex1solution.java
+++ day14/optionalex1solution.java
@@ -1,9 +1,16 @@
 package day14;
 
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class optionalex1solution {
     public static void main(String[] args){
+
+        List<Integer> numbers = Arrays.asList(10,20,25,30);
+
+        Optional<Integer> result = Optional.of(35);
+        System.out.println(result.isPresent());
         
     }
 }
