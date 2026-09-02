0e20e265f57aabd317e778a27424f8b991005350
Abin-ms <abinn.mss@gmail.com>
2026-09-02 15:58:27 +0530

    program showing the use of Optioanl in java

--- day14/optionalex1solution.java
+++ day14/optionalex1solution.java
@@ -1,19 +1,25 @@
 package day14;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 public class optionalex1solution {
     public static void main(String[] args){
 
-        List<Integer> numbers = Arrays.asList(10,20,25,30);
+        // List<Integer> numbers = Arrays.asList(10,20,25,30);
 
-        Optional<Integer> result = Optional.of(35);
-        System.out.println(result.isPresent());
-        System.out.println(numbers);
+        // Optional<Integer> result = Optional.of(35);
+        // System.out.println(result.isPresent());
+        // System.out.println(numbers);
+        String[] words = new String[10];
 
-        
-        
+        Optional <String> result = Optional.ofNullable(words[5]);
+
+        if(result.isPresent()){
+            String word = words[5].toLowerCase();
+        System.out.println(word);
+
+        }
+        else{
+            System.out.println("Word is empty");
+        }
     }
 }
