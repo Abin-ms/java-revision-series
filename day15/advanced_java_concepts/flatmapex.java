cf629201d84d028cbb73260f5e4adfb4fcce8301
Abin-ms <abinn.mss@gmail.com>
2026-09-07 19:56:31 +0530

    flatMap() example programs

---
+++ day15/advanced_java_concepts/flatmapex.java
@@ -0,0 +1,18 @@
+package day15.advanced_java_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class flatmapex {
+    public static void main(String[] args) {
+        List<List<Integer>> numbers = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9));
+
+        numbers.stream()
+                .flatMap(list -> list.stream())
+                .forEach(n -> System.out.println(n));
+
+    }
+}
---
+++ day15/advanced_java_concepts/flatmapex2.java
@@ -0,0 +1,18 @@
+package day15.advanced_java_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class flatmapex2 {
+    public static void main(String[] args){
+        List<List<Integer>> numbers = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9));
+
+                numbers.stream()
+                .flatMap(list -> list.stream())
+                .filter( n -> n % 2 == 0)
+                .forEach(n -> System.out.println(n));
+    }
+}
