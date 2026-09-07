bc65d77a10bc6d0608a912e140fb768c8fc70107
Abin-ms <abinn.mss@gmail.com>
2026-09-07 22:56:41 +0530

    example use case of groupingBy() without any conditions

---
+++ day15/advanced_java_concepts/groupingByex3.java
@@ -0,0 +1,16 @@
+package day15.advanced_java_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class groupingByex3 {
+    public static void main(String[] args){
+        List<String> names = Arrays.asList("Abin","Rahul","Alex","Sam","David");
+
+        Map<Integer,List<String>> result = names.stream()
+        .collect(Collectors.groupingBy(name -> name.length()));
+        System.out.println(result);
+    }
+}
