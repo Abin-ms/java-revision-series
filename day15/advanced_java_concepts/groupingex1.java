20dbb0b5ffdea5caa67dd310e70fc859a1201583
Abin-ms <abinn.mss@gmail.com>
2026-09-07 23:31:00 +0530

    example of joining()

---
+++ day15/advanced_java_concepts/groupingex1.java
@@ -0,0 +1,17 @@
+package day15.advanced_java_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class groupingex1 {
+    public static void main(String[] args){
+        List<String> names = Arrays.asList(
+    "Abin", "Rahul", "John", "Alex", "David"
+);
+
+String result = names.stream()
+.collect(Collectors.joining("-"));
+System.out.println(result);
+    }
+}
