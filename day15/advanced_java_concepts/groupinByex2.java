4274f6fea416a5bb21e1c9ccc8f89db4b1a1af88
Abin-ms <abinn.mss@gmail.com>
2026-09-07 20:59:53 +0530

    corrected an error

--- day15/advanced_java_concepts/groupinByex2.java
+++ day15/advanced_java_concepts/groupinByex2.java
@@ -9,7 +9,9 @@ public class groupinByex2 {
     public static void main(String[] args){
         List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,8,9,10,12,34,55);
 
-        Map<Integer,List<Integer>> result = numbers.stream()
+        Map<Boolean,List<Integer>> result = numbers.stream()
         .collect(Collectors.groupingBy(n -> n % 2 == 0));
+
+        System.out.println(result);
     }
 }
