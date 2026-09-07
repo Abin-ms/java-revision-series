6ee011117e005a7eda454bec13d1fbc92dbddd65
Abin-ms <abinn.mss@gmail.com>
2026-09-07 22:38:25 +0530

    Example of groupingBy()

--- day15/advanced_java_concepts/groupinByex2.java
+++ day15/advanced_java_concepts/groupinByex2.java
@@ -9,8 +9,12 @@ public class groupinByex2 {
     public static void main(String[] args){
         List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,8,9,10,12,34,55);
 
+        // Map<Integer,List<Integer>> result = numbers.stream()
+        // .collect(Collectors.groupingBy(n -> n % 2));
+
         Map<Boolean,List<Integer>> result = numbers.stream()
         .collect(Collectors.groupingBy(n -> n % 2 == 0));
+
 
         System.out.println(result);
     }
