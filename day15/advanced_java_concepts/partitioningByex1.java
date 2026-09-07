61d64bd4819320242492c06119c68e679a3a100f
Abin-ms <abinn.mss@gmail.com>
2026-09-07 23:11:29 +0530

    Example for partitioningBy()

---
+++ day15/advanced_java_concepts/partitioningByex1.java
@@ -0,0 +1,17 @@
+package day15.advanced_java_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class partitioningByex1 {
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,8,9,10,12,34,55);
+
+
+        Map<Boolean,List<Integer>> result = numbers.stream()
+        .collect(Collectors.partitioningBy( n -> n % 2 == 0));
+        System.out.println(result);
+    }
+}
