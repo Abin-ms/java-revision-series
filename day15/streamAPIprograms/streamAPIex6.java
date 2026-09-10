ca4d625bad75d3d4f10801cfd7d108a0c5cf9237
Abin-ms <abinn.mss@gmail.com>
2026-09-11 00:20:55 +0530

    stram program to find the max / highest element from the list

---
+++ day15/streamAPIprograms/streamAPIex6.java
@@ -0,0 +1,18 @@
+package day15.streamAPIprograms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class streamAPIex6 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(
+                45, 12, 89, 34, 67, 23);
+
+        Optional<Integer> result = numbers.stream()
+                .reduce((a, b) -> a > b ? a : b);
+
+        System.out.println(result);
+
+    }
+}
