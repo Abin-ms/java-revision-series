bbf72c78f768e1b4e6a937555c2a789effc92c63
Abin-ms <abinn.mss@gmail.com>
2026-09-11 08:39:20 +0530

    streamAPI example to group elements according to 'n % 3'

---
+++ day15/streamAPIprograms/streamAPIex14.java
@@ -0,0 +1,18 @@
+package day15.streamAPIprograms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class streamAPIex14 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(
+                10, 11, 12, 13, 14, 15, 16, 17);
+
+        Map<Integer, List<Integer>> result = numbers.stream()
+                .collect(Collectors.groupingBy(n -> n % 3));
+
+        System.out.println(result);
+    }
+}
