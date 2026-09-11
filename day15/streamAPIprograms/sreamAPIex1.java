de67c251f232182a86df008d841b0db5c35ec443
Abin-ms <abinn.mss@gmail.com>
2026-09-11 08:30:11 +0530

    changed the name of the example file

--- day15/streamAPIprograms/sreamAPIex1.java
+++ day15/streamAPIprograms/sreamAPIex1.java
@@ -3,7 +3,7 @@ package day15.streamAPIprograms;
 import java.util.Arrays;
 import java.util.List;
 
-public class streampgm2 {
+public class sreamAPIex1 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(5, 12, 7, 20, 33, 40, 15, 8);
 
